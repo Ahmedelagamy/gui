7da6ab7ed728b162856c08164ebd9b1f31b27140
Ahmed Mohsen El Agamy <58571185+Ahmedelagamy@users.noreply.github.com>
2022-08-30 12:35:04 +0200

    Update main.py
    Adding Sentiment Analysis

--- main.py
+++ main.py
@@ -41,7 +41,14 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   df = pd.read_excel(uploaded_file)
   st.write(df)
-    
+
+df['TextBlob_Subjectivity'] = df['review-text'].apply(get_subjectivity)
+df['TextBlob_Polarity'] = df['review-text'].apply(get_polarity)
+
+# Applying Analysis Function
+
+df['TextBlob_Analysis'] = df['TextBlob_Polarity'].apply(get_analysis)
+
 # Data preprocessing
 # noinspection PyArgumentList
 # Splitting data
