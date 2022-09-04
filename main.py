447b2c9f4959f0501d597a6d934361428ad568de
Ahmed Mohsen El Agamy <58571185+Ahmedelagamy@users.noreply.github.com>
2022-09-04 13:56:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ with tab1:
     st.subheader('Positive Reviews')
     st.dataframe(good_reviews)
     st.image(freq_words(good_reviews.groupby('asin')['review-text'].sum())
-    umap_model = UMAP(n_neighbors=15, n_components=5, min_dist=0.0, metric='cosine')
+    
     topic_model_1 = BERTopic(language= 'en', n_gram_range= (2,3), diversity=.6, verbose=True, nr_topics='auto')
 
     good_model = topic_model_1.fit(good_reviews)
