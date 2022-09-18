01813c77c79f0788b1da7015edc9727c03283ab4
Ahmed El Agamy <elagamy515@gmail.com>
2022-09-18 11:08:22 +0200

    Update .gitignore, gui.iml, and 7 more files...

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import streamlit as st
 from bertopic import BERTopic
 from textblob import TextBlob
 from umap import UMAP
+import hdbscan
 import re
 import nltk
 nltk.download('stopwords')
@@ -20,19 +21,6 @@ from nltk.stem.wordnet import WordNetLemmatizer
 from langdetect import detect
 
 # function to plot most frequent terms
-def freq_words(x, terms = 30):
-  all_words = ' '.join([text for text in x])
-  all_words = all_words.split()
-
-  fdist = FreqDist(all_words)
-  words_df = pd.DataFrame({'word':list(fdist.keys()), 'count':list(fdist.values())})
-
-  # selecting top 20 most frequent words
-  d = words_df.nlargest(columns="count", n = terms) 
-  plt.figure(figsize=(20,5))
-  ax = sns.barplot(data=d, x= "word", y = "count")
-  ax.set(ylabel = 'Count')
-  plt.show()
 
 # Functions for interpreting TextBlob analysis
 def get_subjectivity(text):
